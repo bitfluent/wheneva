82393bacb8e4a9129ab5df0f7069c91630d1073d
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2009-11-04 22:03:44 +0800

    Reorganize the routes to be split between non-subdomain and subdomain routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :appointments
-  map.resources :accounts
-  map.root :controller => "home", :action => "show"
+  # scoped urls - http://morib.meet.me/appointments/new
+  map.with_options :conditions => { :subdomain => /./ } do |account|
+    account.resources :appointments
+    account.root :controller => "appointments", :action => "new"
+  end
+
+  # top-level urls - http://meet.me/accounts/new
+  map.with_options :conditions => { :subdomain => false } do |meetme|
+    meetme.resources :accounts
+    meetme.root :controller => "home", :action => "show"
+  end
 end
