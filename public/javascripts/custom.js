9a5406e3927bb0a9d46f0f14c7a25a0b44e32ff7
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2009-11-04 16:27:08 +0800

    Import the appointments/new template

---
+++ public/javascripts/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	Cufon.replace('h1');
+	Cufon.replace('h2');
+	Cufon.replace('h3');
+	Cufon.replace('h4');
+	Cufon.replace('h5');
+	Cufon.replace('h6');					   
+});
+
+			
