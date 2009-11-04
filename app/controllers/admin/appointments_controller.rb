a46c2b40a78a4b8778ef25b76dbc6b2eff2ebbfa
Arzumy MD <hello@ar.zu.my>
2009-11-05 00:31:29 +0800

    admin appointments index done without link to view

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -3,7 +3,7 @@ class Admin::AppointmentsController < InheritedResources::Base
   before_filter :get_week, :only => :index
 
   def index
-    @appointments = Appointment.weekly_appointments(@week)
+    @appointments = Appointment.weekly_appointments(@week).group_by { |t| t.confirmed_date.beginning_of_day.day}.sort
     @pendings = Appointment.pendings
   end
   
--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,3 +1,3 @@
 Time::DATE_FORMATS[:appointment_date] = lambda { |time| time.strftime("%A, %B #{time.day.ordinalize}") }
-Time::DATE_FORMATS[:appointment_date_short] = "%a %b %y" #th Nov 09
+Time::DATE_FORMATS[:appointment_date_short] = "%d %b %y" #th Nov 09
 Time::DATE_FORMATS[:appointment_time] = "%l %p"
