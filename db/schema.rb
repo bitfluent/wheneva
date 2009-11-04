55b4abad21fb4c8b114754052deed58112c67120
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2009-11-05 00:35:43 +0800

    Update to latest schema

--- db/schema.rb
+++ db/schema.rb
@@ -46,4 +46,7 @@ ActiveRecord::Schema.define(:version => 20091104162926) do
     t.datetime "updated_at"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
+
 end
