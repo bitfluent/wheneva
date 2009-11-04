fbef4043aa766617aaa034f5400a3cc236f4417c
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2009-11-04 16:46:18 +0800

    Hook up js to select timeslots

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-	Cufon.replace('h1');
-	Cufon.replace('h2');
-	Cufon.replace('h3');
-	Cufon.replace('h4');
-	Cufon.replace('h5');
-	Cufon.replace('h6');					   
-});
+  Cufon.replace('h1');
+  Cufon.replace('h2');
+  Cufon.replace('h3');
+  Cufon.replace('h4');
+  Cufon.replace('h5');
+  Cufon.replace('h6');
 
-			
+  $(".date_confirm").text($("#date_picker li a.selected").text());
+
+  $("#date_picker li a").click(function() {
+    $(".date_confirm").text($(this).text());
+    $("#date_picker li a.selected").removeClass("selected");
+    $(this).addClass("selected");
+    return false;
+  });
+});
