ad2089cbf59812bfd0577a9cc42cf42b5578df1d
Arzumy MD <hello@ar.zu.my>
2009-11-05 00:58:31 +0800

    fixed problem when no appointments, included scoping in admin appointments

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -1,14 +1,25 @@
 class Admin::AppointmentsController < InheritedResources::Base
+  include ApplicationHelper
   before_filter :authenticate_user!
   before_filter :get_week, :only => :index
 
   def index
-    @appointments = Appointment.weekly_appointments(@week).group_by { |t| t.confirmed_date.beginning_of_day.day}.sort
-    @pendings = Appointment.pendings
+    @pendings = end_of_association_chain.pendings
+    index!
   end
   
 protected
   def get_week
     @week = params[:week] || Time.zone.today.cweek
   end
+  
+  def begin_of_association_chain
+    current_account
+  end
+  
+  def collection
+    @appointments ||= end_of_association_chain.weekly_appointments(@week).group_by { |t| t.confirmed_date.beginning_of_day.day}.sort
+  end
+  
+  
 end
