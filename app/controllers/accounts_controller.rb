8c03e05c6598b2f6e9138bea326c0e29f1baa706
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2009-11-05 00:07:51 +0800

    Show the right account name for subdomained pages

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,6 @@
 class AccountsController < InheritedResources::Base
+  layout "home"
+
   def create
     create! { new_user_session_url(:subdomain => @account.subdomain) }
   end
--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,8 @@ class Account < ActiveRecord::Base
   has_many :appointments
   belongs_to :user
   accepts_nested_attributes_for :user
+
+  def to_s
+    self.title
+  end
 end
