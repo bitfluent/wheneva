df4919078002271e30ca17408549d69ba48dcbe2
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2009-11-05 07:06:00 +0800

    Redirect admins to the dashboard if they are signed in

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,13 @@
 class AppointmentsController < InheritedResources::Base
   include ApplicationHelper
+  before_filter :redirect_to_admin
 
 protected
   def begin_of_association_chain
     current_account
+  end
+
+  def redirect_to_admin
+    redirect_to admin_appointments_url and return if user_signed_in?
   end
 end
