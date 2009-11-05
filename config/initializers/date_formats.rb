b3853ff274b08e71f5caf6dc6b8acaa3e70c9ed8
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2009-11-05 08:06:07 +0800

    Use a dynamic date for appointments/new

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -2,3 +2,4 @@ Time::DATE_FORMATS[:appointment_date] = lambda { |time| time.strftime("%A, %B #{
 Time::DATE_FORMATS[:appointment_date_short] = "%d %b %y" #th Nov 09
 Time::DATE_FORMATS[:appointment_time] = "%l %p"
 Time::DATE_FORMATS[:slot] = lambda { |time| time.strftime("%b #{time.day.ordinalize} at #{time.hour > 12 ? time.hour - 12 : time.hour}#{time.hour > 11 ? "pm" : "am"}") } # Nov 5th at 9am
+Time::DATE_FORMATS[:current_date] = lambda { |time| time.strftime("#{time.day.ordinalize} %b %Y") } # 4th Nov 2009
